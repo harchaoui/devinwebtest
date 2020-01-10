4cf80b50da43bb3eccb6adc5153662ef6f6af42a
abdelali1 <abdelali@fuzart.com>
2020-01-10 00:36:47 +0000

    create the city model and migration

--- app/Http/Controllers/CitiesController.php
+++ app/Http/Controllers/CitiesController.php
@@ -6,5 +6,10 @@ use Illuminate\Http\Request;
 
 class CitiesController extends Controller
 {
-    //
+    //Create store function to add a city(id,name.slug)
+    // and handle the api => POST /api/city/
+    public function store(Request $request) {
+        
+
+    }
 }
